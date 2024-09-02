a98cced55f1d727e0fb32dec043424e5c129a728
Leticia Correia <leticiacorreiatsi@gmail.com>
2024-09-02 18:11:22 -0300

    Alterações do link do youtube e botão de doação

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,63 @@
 <template>
-  <div class="flex flex-col py-12 bg-[#ece3ca] text-[#711E16] justify-center items-center">
+  <div class="flex flex-col py-2 bg-[#ece3ca] text-[#711E16] justify-center items-center">
+
+    <div class="carousel w-5/5">
+      <div id="item1" class="carousel-item w-full">
+        <img
+          src="~/assets/img/background.jpg"
+          class="w-full" />
+      </div>
+      <div id="item2" class="carousel-item w-full">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+          class="w-full" />
+      </div>
+    </div>
+    <!--<div class="flex w-full justify-center gap-2 py-2">
+      <a href="#item1" class="btn btn-xs">1</a>
+      <a href="#item2" class="btn btn-xs">2</a>
+    </div> -->
+
+    <div class="w-3/5 text-center py-12 text-2xl font-semibold">
+      <p> Os Arautos do Evangelho são uma Associação Internacional de Fiéis de Direito Pontifício, 
+          a primeira a ser erigida pela Santa Sé no terceiro Milênio, o que ocorreu por ocasião 
+          da festa litúrgica da Cátedra de São Pedro em 22/02/2001. 
+      </p>
+    </div>
     
-    <div class="flex flex-col gap-5 justify-center items-center">
-        <p class="text-5xl italic font-medium my-4">Conheça um pouco da nossa história: </p>
-        <video width="720" height="440" controls>
-            <source src="https://youtu.be/gF927Rv4HSg" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
+    <div class="flex flex-col w-4/5 p-10 gap-5 justify-center items-center">
+        <p class="text-5xl text-center italic font-semibold my-4">Construção da Capela Nossa Senhora do Bom Sucesso</p>
+        <iframe 
+          width="720" 
+          height="445" 
+          src="https://www.youtube.com/embed/gF927Rv4HSg?si=NHUv-1bbss5WhYbr" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen>
+        </iframe>
     </div>
    
-    <div class="w-full h-28 text-center italic py-12 text-5xl">
-        Conheça mais sobre os Arautos do Evangelho 
-    </div>
-
-    <div class="flex py-30 w-4/5 md:w-3/4 gap-5 justify-center text-[#711E16]">
-        <div class="flex w-1/2 justify-end">
-            <img src="~/assets/img/background.jpg" class="object-contain lg:object-cover h-26 md:h-48 w-102" alt="">
+    <div class="flex flex-col py-30 w-4/5 gap-10 justify-center text-[#711E16]">
+        <div class="w-full h-28 text-center italic py-12 text-4xl">
+            Ajude-nos neste projeto:
         </div>
-        <div class="w-1/2 mb-10 ps-3">
-        <time class="font-mono italic">2001</time>
-        <div class="text-xl font-black">Início dos Arautos do Evangelho </div>
-            Os Arautos do Evangelho são uma Associação Internacional de Fiéis de Direito Pontifício, <br>
-            a primeira a ser erigida pela Santa Sé no terceiro Milênio, o que ocorreu por ocasião <br>
-            da festa litúrgica da Cátedra de São Pedro em 22/02/2001.
+
+        <div class="flex w-full justify-center py-2 text-2xl font-semibold">
+          <div class="flex w-2/4 justify-end items-end">
+            <p> Faça parte desta obra de Maria, <br>ajude à construção desta casa de Deus!</p>
+          </div>
+          <div class="flex items-end ps-10 w-2/4">
+            <form action="" method="post" target="_top">
+              <input name="hosted_button_id" type="hidden" value="">
+              <span>
+                <input title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" type="image"></span><span><img loading="lazy" decoding="async" src="https://www.paypal.com/en_BR/i/scr/pixel.gif" alt="" width="1" height="1" border="0">&nbsp;
+              </span>
+            </form>
+          </div>
         </div>
         <hr />
-    
     </div>
   </div>
 </template>
